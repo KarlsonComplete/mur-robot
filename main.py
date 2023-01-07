f85ad6740603475c18e2463b44c72a21e4505928
Кирилл Храмов <hramovkirill01@gmail.com>
2023-01-07 11:06:14 +0300

    - Добавили реализацию ПД-регулятора

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 import pymurapi as mur
+import time
 
 auv = mur.mur_init()
 
-while True:
-    auv.set_motor_power(0, 100)
-    auv.set_motor_power(1, 100)
+prev_time = 0
+prev_error = 0.0
 
+
+def clamp(v, max_v, min_v):
+    if v > max_v:
+        return max_v
+    if v < min_v:
+        return min_v
+
+    return v
+
+
+def keep_depth(value):
+    global prev_time
+    global prev_error
+
+    current_time = int(round(time.time() * 1000))
+
+    error = auv.get_depth() - value
+
+    power_2 = 0
+    power_3 = 0
+
+    power_value = error * 70
+    diff_value = 5 / (current_time - prev_time) * (error - prev_error)
+
+    power_2 = clamp(power_value + diff_value, 100, -100)
+    power_3 = clamp(power_value + diff_value, 100, -100)
+
+    auv.set_motor_power(2, power_2)
+    auv.set_motor_power(3, power_3)
+
+    prev_time = current_time
+    prev_error = error
+
+
+while True:
+    keep_depth(2)
+    time.sleep(0.03)
